f72a7a462b12daaa2e55745b19215d600985f8ca
Thomas Brushel <thomas@wantable.com>
2018-04-22 21:39:34 -0500

    Add files via upload

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,7 @@
+// To create a functional component. Do rfc then 
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const NavBar = () => <AppBar title="PixaBay Image Finder" />;
+
+export default NavBar;
